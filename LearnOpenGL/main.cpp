4dfb7356add5a86c3f0fcd39075545080189b852
IAKSH <1248675212@qq.com>
2023-02-22 21:31:15 +0800

    a cool cool rectangle

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -16,19 +16,22 @@ static void shaderInitialize()
 		"layout (location = 0) in vec3 aPos;\n"
 		"layout (location = 1) in vec3 aColor;\n"
 		"out vec3 ourColor;\n"
+		"out vec4 ourPos;\n"
 		"uniform vec2 xyoffset;"
 		"void main()\n"
 		"{\n"
 		"    gl_Position = vec4(aPos.x + xyoffset.x,aPos.y + xyoffset.y,aPos.z,1.0);\n"
+		"    ourPos = gl_Position;\n"
 		"    ourColor = aColor;\n"
 		"}\0";
 
 	const char* fragmentShaderSource = "#version 330 core\n"
 		"out vec4 FragColor;\n"
 		"in vec3 ourColor;\n"
+		"in vec4 ourPos;\n"
 		"void main()\n"
 		"{\n"
-		"    FragColor = vec4(ourColor, 1.0);\n"
+		"    FragColor = vec4(ourColor.x - (ourPos.x / 2.0),ourColor.y - (ourPos.y / 2.0),ourColor.z - (ourPos.z / 1.5), 1.0);\n"
 		"}\0";
 
 	// vertex shader
