864b0d52c3cf5fc6057a00b1ed74a2e6f2edbe88
IAKSH <1248675212@qq.com>
2023-02-24 17:33:32 +0800

    brain meme

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -1,7 +1,45 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
 #include <iostream>
 #include <cinttypes>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+// ---------------------------------------------------
+
+static uint32_t texture[2];
+
+static void loadTexture()
+{
+	stbi_set_flip_vertically_on_load(true);
+
+	const char* path[2] = { "idk.png","idk1.png" };
+
+	glGenTextures(2, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	for (int i = 0; i < 2; i++)
+	{
+		int w, h, channels;
+		unsigned char* data = stbi_load(path[i], &w, &h, &channels, 0);
+		if (!data)
+		{
+			std::cerr << "[ERROR] Can't load " << path[i] << std::endl;
+			abort();
+		}
+
+		glBindTexture(GL_TEXTURE_2D, texture[i]);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		stbi_image_free(data);
+	}
+}
 
 // ---------------------------------------------------
 
@@ -15,23 +53,29 @@ static void shaderInitialize()
 	const char* vertexShaderSource = "#version 330 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
 		"layout (location = 1) in vec3 aColor;\n"
+		"layout (location = 2) in vec2 aTexCoord;\n"
 		"out vec3 ourColor;\n"
+		"out vec2 TexCoord;\n"
 		"out vec4 ourPos;\n"
 		"uniform vec2 xyoffset;"
 		"void main()\n"
 		"{\n"
-		"    gl_Position = vec4(aPos.x + xyoffset.x,aPos.y + xyoffset.y,aPos.z,1.0);\n"
+		"    gl_Position = vec4(aPos.x + (xyoffset.x / 2),aPos.y + (xyoffset.y / 2),aPos.z,1.0);\n"
 		"    ourPos = gl_Position;\n"
 		"    ourColor = aColor;\n"
+		"    TexCoord = aTexCoord;\n"
 		"}\0";
 
 	const char* fragmentShaderSource = "#version 330 core\n"
 		"out vec4 FragColor;\n"
 		"in vec3 ourColor;\n"
 		"in vec4 ourPos;\n"
+		"in vec2 TexCoord;\n"
+		"uniform sampler2D texture0;\n"
+		"uniform sampler2D texture1;\n"
 		"void main()\n"
 		"{\n"
-		"    FragColor = vec4(ourColor.x - (ourPos.x / 2.0),ourColor.y - (ourPos.y / 2.0),ourColor.z - (ourPos.z / 1.5), 1.0);\n"
+		"    FragColor = mix(texture(texture0,TexCoord),texture(texture1,TexCoord),0.5);\n"
 		"}\0";
 
 	// vertex shader
@@ -75,6 +119,10 @@ static void shaderInitialize()
 	// start shader program
 	glUseProgram(shaderProgram);
 
+	// bind GL_TEXTUREx
+	glUniform1i(glGetUniformLocation(shaderProgram, "texture0"), 0);// "texture0" -> GL_TEXTURE0
+	glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 1);// "texture1" -> GL_TEXTURE1
+
 	// delete vertexShader & fragmentShader
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
@@ -88,11 +136,11 @@ static void drawInitialize()
 	// prepare our vertex data
 	const float vertexData[] =
 	{
-		// vertex				color
-		0.5f,	0.5f,	0.0f,	0.0f,	1.0f, 0.0f, // right-up		0
-		0.5f,	-0.5f,	0.0f,	1.0f,	0.0f, 0.0f, // right-down	1
-		-0.5f,	-0.5f,	0.0f,	0.0f,	1.0f, 1.0f,	// left-down	2
-		-0.5f,	0.5f,	0.0f,	0.0f,	0.0f, 1.0f	// left-up		3
+		// vertex				color					texCoord
+		0.5f,	0.5f,	0.0f,	0.0f,	1.0f, 0.0f,		1.0f,1.0f,	// right-up		0
+		0.5f,	-0.5f,	0.0f,	1.0f,	0.0f, 0.0f,		1.0f,0.0f,	// right-down	1
+		-0.5f,	-0.5f,	0.0f,	0.0f,	1.0f, 1.0f,		0.0f,0.0f,	// left-down	2
+		-0.5f,	0.5f,	0.0f,	0.0f,	0.0f, 1.0f,		0.0f,1.0f	// left-up		3
 	};
 
 	// prepare our indices data
@@ -123,11 +171,14 @@ static void drawInitialize()
 
 	// set vertex attrib pointer
 	// vertex position attrib
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	// vertex color attrib
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+	// texture coord attrib
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 }
 
 static void draw()
@@ -135,6 +186,12 @@ static void draw()
 	// update uniform
 	int vertexColorLocation = glGetUniformLocation(shaderProgram, "xyoffset");
 	glUniform2f(vertexColorLocation, sin(glfwGetTime()), cos(glfwGetTime()));
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture[0]);
+
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, texture[1]);
 
 	glBindVertexArray(vao);
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
@@ -174,6 +231,8 @@ static void mainLoop()
 
 	// test
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	loadTexture();
 
 	while (!glfwWindowShouldClose(window))
 	{
