3fef78b58a68bd31636155366c93f16b38810809
IAKSH <1248675212@qq.com>
2023-02-25 21:41:24 +0800

    small format

--- LearnOpenGL/fragmentshader.glsl
+++ LearnOpenGL/fragmentshader.glsl
@@ -1,10 +1,13 @@
 #version 330 core
 out vec4 FragColor;
+
 in vec3 ourColor;
 in vec4 ourPos;
 in vec2 TexCoord;
+
 uniform sampler2D texture0;
 uniform sampler2D texture1;
+
 void main()
 {
     FragColor = mix(texture(texture0,TexCoord),texture(texture1,TexCoord),0.5) * vec4(ourColor.x - (ourPos.x / 2.0),ourColor.y - (ourPos.y / 2.0),ourColor.z - (ourPos.z / 1.5), 0.1);
--- LearnOpenGL/vertexshader.glsl
+++ LearnOpenGL/vertexshader.glsl
@@ -2,10 +2,13 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 layout (location = 2) in vec2 aTexCoord;
+
 out vec3 ourColor;
 out vec2 TexCoord;
 out vec4 ourPos;
+
 uniform vec2 xyoffset;
+
 void main()
 {
     gl_Position = vec4(aPos.x + (xyoffset.x / 2),aPos.y + (xyoffset.y / 2),aPos.z,1.0);
